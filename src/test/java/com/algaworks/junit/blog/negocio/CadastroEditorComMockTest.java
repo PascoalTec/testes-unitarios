6a0bc92084cdf647ee95250d89480f7d7bc52b30
Leonardo <leonardopascoall@hotmail.com>
2025-07-14 09:20:48 -0300

    att 14/07

--- src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
@@ -1,15 +1,15 @@
 package com.algaworks.junit.blog.negocio;
 
 import com.algaworks.junit.blog.armazenamento.ArmazenamentoEditor;
+import com.algaworks.junit.blog.exception.RegraNegocioException;
 import com.algaworks.junit.blog.modelo.Editor;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,7 +17,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 public class CadastroEditorComMockTest {
 
-    Editor editor;
+    @Spy
+    Editor editor = new Editor(null, "Alex", "alex@email.com", BigDecimal.TEN, true);
+
+    @Captor
+    ArgumentCaptor<Mensagem> mensagemArgumentCaptor;
 
     @Mock
     GerenciadorEnvioEmail gerenciadorEnvioEmail;
@@ -30,8 +34,6 @@ public class CadastroEditorComMockTest {
 
     @BeforeEach
     void init() {
-        editor = new Editor(null, "Alex", "alex@email.com", BigDecimal.TEN, true);
-
         Mockito.when(armazenamentoEditor.salvar(Mockito.any(Editor.class)))
                 .thenAnswer(invocacao -> {
                     Editor editorPassado = invocacao.getArgument(0, Editor.class);
@@ -51,5 +53,55 @@ public class CadastroEditorComMockTest {
     void Dado_um_editor_valido_Quando_criar_Entao_deve_chamar_metodo_salvar_do_armazenamento() {
         cadastroEditor.criar(editor);
         Mockito.verify(armazenamentoEditor, Mockito.times(1)).salvar(Mockito.eq(editor));
+    }
+
+    @Test
+    void Dado_um_editor_valido_Quando_criar_e_lancar_exception_ao_salvar_Entao_nao_deve_enviar_email() {
+        Mockito.when(armazenamentoEditor.salvar(editor)).thenThrow(new RuntimeException());
+        assertAll("Não deve enviar e-mail quando lançar exception do armazenamento",
+                () -> assertThrows(RuntimeException.class, () -> cadastroEditor.criar(editor)),
+                () -> Mockito.verify(gerenciadorEnvioEmail, Mockito.never()).enviarEmail(Mockito.any())
+                );
+    }
+
+
+    @Test
+    void Dado_um_editor_valido_Quando_cadastrar_Entao_deve_enviar_email_com_destino_ao_editor() {
+        Editor editorSalvo = cadastroEditor.criar(editor);
+
+        Mockito.verify(gerenciadorEnvioEmail).enviarEmail(mensagemArgumentCaptor.capture());
+
+        Mensagem mensagem = mensagemArgumentCaptor.getValue();
+
+        assertEquals(editorSalvo.getEmail(), mensagem.getDestinatario());
+    }
+
+
+    @Test
+    void Dado_um_editor_valido_Quando_cadastrar_Entao_deve_verificar_o_email() {
+        cadastroEditor.criar(editor);
+
+        Mockito.verify(editor, Mockito.atLeast(1)).getEmail();
+    }
+
+    @Test
+    void Dado_um_editor_com_email_existente_Quando_cadastrar_Entao_deve_lancar_exception() {
+        Mockito.when(armazenamentoEditor.encontrarPorEmail("alex@email.com"))
+                .thenReturn(Optional.empty())
+                .thenReturn(Optional.of(editor));
+
+        Editor editorComEmailExistente = new Editor(null, "Alex", "alex@email.com", BigDecimal.TEN, true);
+
+        cadastroEditor.criar(editor);
+        assertThrows(RegraNegocioException.class, () -> cadastroEditor.criar(editorComEmailExistente));
+    }
+
+    @Test
+    void Dado_um_editor_valido_Quando_cadastrar_Entao_deve_enviar_email_apos_salvar() {
+        cadastroEditor.criar(editor);
+
+        InOrder inOrder = Mockito.inOrder(armazenamentoEditor, gerenciadorEnvioEmail);
+        inOrder.verify(armazenamentoEditor, Mockito.times(1)).salvar(editor);
+        inOrder.verify(gerenciadorEnvioEmail, Mockito.times(1)).enviarEmail(Mockito.any(Mensagem.class));
     }
 }
